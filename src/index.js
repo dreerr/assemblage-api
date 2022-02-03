fbf45bfdb65a12f925ec58de64a366dc92602324
Julian <dreerr@gmail.com>
2022-02-03 23:14:01 +0100

    better logging

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import config from "./config.js"
 import server from "./server.js"
 import contractListener from "./blockchain/contract-listener.js"
 import contractScheduler from "./blockchain/contract-scheduler.js"
+import { logger } from "./utils/logger.js"
 
 if (config.unsafeHttps) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 }
+
+logger.info(`########### SERVER STARTING ###########`)
 
 server.listen(config.port, () => {
   contractListener()
