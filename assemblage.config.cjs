cf5f0930312f5c3c8b124c04ff7590e2a78f2615
dreerr <dreerr@gmail.com>
2022-06-01 03:06:18 +0200

    add cronjob to restart

--- assemblage.config.cjs
+++ assemblage.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
       script: "./src/index.js",
       watch: ["src", "contracts"],
       watch_delay: 1000,
+      cron_restart: '0 0 * * *',
       instance_var: "INSTANCE_ID",
       env: {
         NODE_ENV: "production",
