2bd26e175931807e0f8218bda686eea456ab10b1
Julian <dreerr@gmail.com>
2022-08-20 12:42:10 +0200

    info logging that backup is disabled

--- src/utils/rsync.js
+++ src/utils/rsync.js
@@ -3,6 +3,10 @@ import config from "../config.js"
 import { logger } from "../utils/logger.js"
 import { sendText } from "../utils/telegram.js"
 
+const enabled = config.backupDest !== undefined && config.backupDest !== null
+
+if (!enabled) logger.info("rsync backup/upload is disabled!")
+
 const backup = new Rsync()
   .shell("ssh")
   .flags("az")
@@ -10,7 +14,7 @@ const backup = new Rsync()
   .destination(config.backupDest)
 
 export default () => {
-  if (config.backupDest !== undefined && config.backupDest !== null) {
+  if (enabled) {
     backup.execute((error, code) => {
       if (error !== null) {
         logger.error(`Backup failed! Error: ${error}, Code: ${code}`)
